4e9465cab2a44083b3e4a9dc99dfc87d252da2f9
David Lucca <david.v.lucca@gmail.com>
2023-11-10 16:04:17 +0000

    Started doing styling using images

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,11 +5,13 @@ let PokemonCard = (props) =>{
     const {url,Json_Url,update, setUpdate} = props;
 
     const [thisPokemon, setThisPokemon] = useState({})
+    const [thisPokemonLoading, setThisPokemonLoading] = useState(true)
 
     useEffect(()=>{
         axios.get(url)
         .then((response)=>{
             setThisPokemon(response.data)
+            setThisPokemonLoading(false)
         })
     },[])
 
@@ -20,9 +22,15 @@ let PokemonCard = (props) =>{
 
     }
 
+    !thisPokemonLoading && console.log(thisPokemon.sprites.front_default);
+
     return( // Rendering a pokemon card with info from all pokemons on allPokemonList
         <div>
+            {thisPokemonLoading && <h1>Loading</h1>}
+            {!thisPokemonLoading && <div><img src={thisPokemon.sprites.front_default}></img></div>}
+            <div>
             {thisPokemon.name}
+            </div>
             <button onClick={clickAdd}>+</button>
         </div>
     )
