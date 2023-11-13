898b7549404ad02d6493991e05afb862f3c3f452
Vasco Gaitinha <vascomiguel.gaitinha@gmail.com>
2023-11-13 09:50:36 +0000

    HomePage Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,15 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+
 import './App.css'
-import LeftSide from './components/LeftSide'
-import RightSide from './components/RighSide'
+
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
 
 function App() {
-
-  const Api_Url="https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150"
-  const Json_Url="http://localhost:5005/pokemons"
-
-  const [loading, setLoading] = useState(true)
-  const [allPokemonList, setAllPokemonList] = useState({})
-  const [myPokemonList,setMyPokemonList] = useState({})
-  const [update, setUpdate] = useState(false)
-
-  useEffect(()=>{ //SETTING ALL POKEMON LIST FROM API
-    axios.get(Api_Url)
-      .then((response)=>{
-        setAllPokemonList(response.data.results)
-        setLoading(false)
-      })
-      .catch((error)=> {console.log(error)})
-
-  },[])
-  //_______________________________________________________
-
-
   return (
-    <div>
-      {loading && (<h1>.: LOADING :.</h1>)}
-      {!loading && 
-                  (<div className="main-container"> 
-                        <LeftSide 
-                        myPokemonList={myPokemonList}
-                        setMyPokemonList={setMyPokemonList}
-                        Json_Url={Json_Url}
-                        setUpdate={setUpdate}
-                        update={update}
-                        />
-                        <RightSide 
-                        allPokemonList={allPokemonList}
-                        Json_Url={Json_Url}
-                        update={update}
-                        setUpdate={setUpdate}
-                        />
-                  </div>)}
-
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+    </Routes>
+    )
 }
 
 export default App
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import '../App.css'
+import LeftSide from './LeftSide'
+import RightSide from './RighSide'
+
+function HomePage() {
+
+  const Api_Url="https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150"
+  const Json_Url="http://localhost:5005/pokemons"
+
+  const [loading, setLoading] = useState(true)
+  const [allPokemonList, setAllPokemonList] = useState({})
+  const [myPokemonList,setMyPokemonList] = useState({})
+  const [update, setUpdate] = useState(false)
+
+  useEffect(()=>{ //SETTING ALL POKEMON LIST FROM API
+    axios.get(Api_Url)
+      .then((response)=>{
+        setAllPokemonList(response.data.results)
+        setLoading(false)
+      })
+      .catch((error)=> {console.log(error)})
+
+  },[])
+  //_______________________________________________________
+  return (
+    <div>
+      {loading && (<h1>.: LOADING :.</h1>)}
+      {!loading && 
+        (<div className="main-container"> 
+            <LeftSide 
+            myPokemonList={myPokemonList}
+            setMyPokemonList={setMyPokemonList}
+            Json_Url={Json_Url}
+            setUpdate={setUpdate}
+            update={update}
+            />
+            <RightSide 
+            allPokemonList={allPokemonList}
+            Json_Url={Json_Url}
+            update={update}
+            setUpdate={setUpdate}
+            />
+        </div>)}
+    </div>
+  )
+}
+
+export default HomePage
